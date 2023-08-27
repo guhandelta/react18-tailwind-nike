deae1dfba0a9ea994d82c1810668807580a0cec9
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2023-08-27 16:13:21 -0400

    Completed the basic skeleton of the home page

---
+++ src/sections/index.js
@@ -0,0 +1,10 @@
+import Hero from './Hero'
+import CustomerReviews from './CustomerReviews'
+import PopularProducts from './PopularProducts'
+import Services from './Services'
+import SpecialOffers from './SpecialOffers'
+import Subscribe from './Subscribe'
+import SuperQuality from './SuperQuality'
+import Footer from './Footer'
+
+export { CustomerReviews, PopularProducts, Services, SpecialOffers, Subscribe, SuperQuality, Footer }
