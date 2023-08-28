624c417d5d74baeaeb5cf133a06289e54984dd95
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2023-08-28 16:03:07 -0400

    Added a Servies section

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Nav from './Nav'
 import Button from './Button'
 import ShoeCard from './ShoeCard'
 import PopularProduct from './PopularProduct'
+import ServiceCard from './ServiceCard'
 
-export { Nav, Button, ShoeCard, PopularProduct }
+export { Nav, Button, ShoeCard, PopularProduct, ServiceCard }
